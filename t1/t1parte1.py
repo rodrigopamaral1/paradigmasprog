c7fc1d27172133ba1769a8b5bb69dd6541932aaa
rodrigopamaral1 <rodrigoufsm@outlook.com>
2017-03-19 19:03:53 -0300

    Update t1parte1.py

--- t1/t1parte1.py
+++ t1/t1parte1.py
@@ -8,32 +8,32 @@ def hasEqHeads(l1,l2): return l1[0] == l2[0]
 # 3.Escreva uma função que receba uma lista de nomes e retorne uma lista com a string "Sr. " adicionada ao início de cada nome. Defina uma função auxiliar para ajudar neste exercício.
 def ConSrAux(strings): return "Sr." + strings
 
-list(map(ConSrAux, ["oi", "ola", "alguem"]))
+def Consr(lista): return list(map(ConSrAux, lista))
 
 # 4.Crie uma função que receba uma string e retorne o número de espaços nela contidos. Defina uma função auxiliar para ajudar neste exercício.
 def CountSpaceAux(c): return c == " "
 
-len(str(filter(CountSpaceAux, "Quantos espacos tem isto?")))
+def CountSpace(string): return len(str(filter(CountSpaceAux, string)))
 
 # 5.Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1 para cada número n da lista. Defina uma função auxiliar para ajudar neste exercício.
 def MatFuncAux(n): return 3*n*2 + 2/n + 1
 
-list(map(MatFuncAux, [1,2,3,4]))
+def MatFunc(lista): return list(map(MatFuncAux, lista))
 
 # 6.Escreva uma função que, dada uma lista de números, retorne uma lista com apenas os que forem negativos. Defina uma função auxiliar para ajudar neste exercício.
 def ReturnNegAux(num): return num < 0
 
-list(filter(ReturnNegAux, [-1,0,1]))
+def ReturnNeg(lista): return list(filter(ReturnNegAux, lista))
 
 # 7.Escreva uma função que receba uma lista de números e retorne somente os que estiverem entre 1 e 100, inclusive. Defina uma função auxiliar para ajudar neste exercício.
 def Return0_100aux(num): return num > 0 and num < 101
 
-list(filter(Return0_100aux, [-1,0,1,99,100,101]))
+def Ret_0_100(lista): return list(filter(Return0_100aux, lista))
 
 # 8.Escreva uma função que receba uma lista de números e retorne somente aqueles que forem pares. Defina uma função auxiliar para ajudar neste exercício.
 def RetParAux(x): return x%2 == 0
 
-list(filter(RetParAux, [1,2,3,4]))
+def RetPar(lista): return list(filter(RetParAux, lista))
 
 # 9.Crie uma função charFound(c,s) que verifique se o caracter c está contido na string. O resultado deve ser True ou False. Você não deve usar o operador in. Defina uma função auxiliar para ajudar neste exercício.
 def aux(c,s): return len(str(filter(lambda x:x == c, s)))
@@ -43,4 +43,4 @@ def charFound(c,s): return aux(c,s) > 0
 # 10.Escreva uma função que receba uma lista de strings e retorne uma nova lista com adição de marcações HTML (p.ex.: <B> e </B>) antes e depois de cada string.
 def html_aux(string): return "<B>" + string + "</B>"
 
-list(map(html_aux, ["oi", "ola"]))
+def html(lista): return list(map(html_aux, lista))
