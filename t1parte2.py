4029981ddd1cefbac38a1ec7be93f5a28aedfe0d
rodrigopamaral1 <rodrigoufsm@outlook.com>
2017-03-18 22:02:52 -0300

    Update t1parte2.py

--- t1parte2.py
+++ t1parte2.py
@@ -11,7 +11,17 @@ def func_mat(lista): return list(map(lambda x:3*x*2 + 2/x + 1, lista))
 def termina_com_a(lista): return list(filter(lambda x:x[len(x)-1] == "a", lista))
 
 # 4.Escreva uma função que, dada uma lista de idades de pessoas no ano atual, retorne uma lista somente com as idades de quem nasceu depois de 1970. Para testar a condição, sua função deverá subtrair a idade do ano atual.
+# >>> idades([20,30,51,57])
+# [20, 30]
+
 def func_1970(lista): return list(filter(lambda x:2017-x > 1970, lista))
 
 # 5. O código abaixo é escrito em Python imperativo. Escreva um código equivalente usando programação funcional.
+
+#numbers = [1, 2, 3, 4]
+#new_numbers = []
+#for number in numbers:
+# new_numbers.append(number * 2)
+#print(new_numbers)
+
 def func_numbers(lista): return list(map(lambda x:x*2, lista))
