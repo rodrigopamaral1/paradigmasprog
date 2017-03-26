66ae572da4d5f902bbae346094c7bae3be48eac0
rodrigopamaral1 <rodrigoufsm@outlook.com>
2017-03-26 17:22:13 -0300

    Update t2parte3.py

--- t2/t2parte3.py
+++ t2/t2parte3.py
@@ -1,1 +1,15 @@
-# Professora queria fazer a parte extra mas infelizmente meu python, versao 3.4.3 nao suporta os import do arquivo.
+# Professora queria fazer a parte extra mas infelizmente meu python, versao 3.4.3 nao suporta os imports do arquivo.
+
+
+
+#>>> from lxml import html, etree
+#Traceback (most recent call last):
+#  File "<pyshell#0>", line 1, in <module>
+#    from lxml import html, etree
+#ImportError: No module named 'lxml'
+#>>> import requests
+#Traceback (most recent call last):
+# File "<pyshell#0>", line 1, in <module>
+#    import requests
+#ImportError: No module named 'requests'
+ 
